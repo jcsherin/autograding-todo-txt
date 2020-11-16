fd07b347ee147155598085457c78ed1fb0111741
Jacob Sherin <jacob@protoship.io>
2020-11-16 17:50:19 +0530

    test helper for cli programs

--- hello.test.js
+++ hello.test.js
@@ -1,45 +1,9 @@
-const { spawn } = require("child_process");
-
-let testStdout = ({ cli, expected, done }) => {
-  // cli : array[2]
-  // array[0] : name of the process
-  // array[1] : arrays of arguments to the process
-  //
-  // Example:
-  // let cli = [node, [`${__dirname}/hello.js`]]
-
-  const process = spawn(...cli);
-
-  // accumulator for streaming data
-  let received = "";
-  process.stdout.on("data", (data) => {
-    received = received + data.toString("utf8");
-  });
-
-  // when finished receiving streaming data
-  // match received against expected
-  process.on("close", (code) => {
-    if (code !== 0) {
-      done(`process exited with code ${code}`);
-    } else {
-      expect(received).toBe(expected);
-      done();
-    }
-  });
-
-  process.stderr.on("data", (data) => {
-    done(data);
-  });
-
-  process.on("error", (error) => {
-    done(error.message);
-  });
-};
+const { expectStdoutToBe } = require("./cliTestHelper");
 
 test("foobar", (done) => {
-  testStdout({
+  expectStdoutToBe({
     cli: [`node`, [`${__dirname}/hello.js`]],
-    expected: "Hello world!\n",
+    expected: "Hello world!",
     done: done,
   });
 });
