eaf53b6b75aaa8c13daf3945f4f6b24218e21322
Jacob Sherin <jacob@protoship.io>
2020-11-16 18:13:55 +0530

    break test

--- hello.test.js
+++ hello.test.js
@@ -3,7 +3,7 @@ const { expectStdoutToBe } = require("./cliTestHelper");
 test("foobar", (done) => {
   expectStdoutToBe({
     cli: [`node`, [`${__dirname}/hello.js`]],
-    expected: "Hello world!",
+    expected: "Hello",
     done: done,
   });
 });
